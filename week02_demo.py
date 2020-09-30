e778f6cefaf65bab956de572cbc450579386ce0f
Giada Sacchi <s2122634@ed.ac.uk>
2020-09-30 14:21:08 +0100

    Edited comment flow

--- week02_demo.py
+++ week02_demo.py
@@ -11,23 +11,40 @@ statement = [[-119.02, -56.54, 1200, -80, -12.99, -550, -167.90, -5.58, -3.54, -
 
 # Your job: pay monthly 0.5% interest to this account.
 
-# Update the account balance each month: OK!
-# Calculate interest each month: OK!
-# Pay the total interest (6 months) to the account:
-# - pay the total (update the balance for the last month)
-# - log that transaction into statement for 6th month.
+# -----------
+
+# Update the account balance each month
 
 def update_balance(month, starting_balance):
     balance = starting_balance + sum(month)
     return balance
 
+
+
+# Calculate interest each month
+
 def calculate_interest(balance, interest_rate):
+    #the following is called "doc-string" and it quickly describes the function
     '''
     Returns total interest paid for a given balance,
     at rate interest_rate (given in %).
     '''
+    # it has to be divided by 100 (i.e. multiplied by 0.01)
+    # so that it actually returns a "fraction"
+    # (e.g. is considers 0.5 as 0.5% and NOT as 50%)
     interest = 0.01 * interest_rate * balance
     return interest
+
+# Check if it works
+example_balance = 200
+example_rate = 0.5
+print(calculate_interest(example_balance, example_rate))
+
+
+
+# Pay the total interest (6 months) to the account:
+# - pay the total (update the balance for the last month)
+# - insert that transaction into statement for 6th month.
 
 def update_statement(statement, initial_balance, interest_rate):
     '''
@@ -35,14 +52,16 @@ def update_statement(statement, initial_balance, interest_rate):
     '''
     balance = initial_balance
     total_interest = 0
-    
+
     for month in statement:
         balance = update_balance(month, balance)
         total_interest = total_interest + calculate_interest(balance, interest_rate)
-    
+
     statement[-1].append(total_interest)
     balance = balance + total_interest
 
     return statement, balance
+
+# -------------
 
 print(update_statement(statement, initial_balance, 0.5))
